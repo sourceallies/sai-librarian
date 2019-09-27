1381a1202fa22652844f81b842fdf2352e481b6e
kfode <kfode@hy-vee.com>
2019-09-27 16:45:58 -0500

    Fixing column issue.

--- src/csvGenerator.js
+++ src/csvGenerator.js
@@ -1,6 +1,6 @@
 const uuid = require('uuid');
 const fs = require('fs');
 
-let file = Array(30).fill(null).map(() => `https://library.sourceallies.com/books/${uuid()}`).join(',');
+let file = Array(30).fill(null).map(() => `https://library.sourceallies.com/books/${uuid()}`).join('\n');
 fs.writeFileSync('./generated-files/testCsv.csv', file);
 console.log('done');
