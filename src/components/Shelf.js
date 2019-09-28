1e572eeac49cd7e901f19653fe11c627e9e096a3
gene <gene@Sources-MacBook-Pro.local>
2019-09-28 15:59:57 -0500

    cancel out of scope

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -4,7 +4,6 @@ const Shelf = ({onCancel, onDone, shelf, title}) => (
     <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start'}}>
         <p>Return {title} to shelf {shelf}</p>
         <div style={{display: 'flex', alignItems: 'center'}}>
-            <button style={{margin: 4}} onClick={onCancel}>Cancel</button>
             <button style={{margin: 4}} onClick={onDone}>Done</button>
         </div>
     </div>
