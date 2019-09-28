1b0eed44575f289a9ce52a1e3e2e2f02fe4767f1
kfode <kfode@hy-vee.com>
2019-09-28 14:13:17 -0500

    CSSing adding a book.

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -2,80 +2,92 @@ import React from 'react';
 import {postBook} from '../utils/postBook';
 
 export default class BookCreate extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      bookTitle: '',
-      isbnNumber: '',
-      shelf: '',
-      'loggedInName': this.props.loggedInName,
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            bookTitle: '',
+            isbnNumber: '',
+            shelf: '',
+            'loggedInName': this.props.loggedInName,
+        };
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
 
-    this.setState({
-      [name]: value
-    });
-  }
+        this.setState({
+            [name]: value
+        });
+    }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    postBook({
-      bookId: this.props.bookId,
-      title: this.state.bookTitle,
-      isbn: this.state.isbnNumber,
-      shelf: this.state.shelf
-    }, this.props.token)
-    .then(() => this.props.history.push('/success'))
-    .catch(err => console.log('Error: ', err));
-  }
+    handleSubmit(event) {
+        event.preventDefault();
+        postBook({
+            bookId: this.props.bookId,
+            title: this.state.bookTitle,
+            isbn: this.state.isbnNumber,
+            shelf: this.state.shelf
+        }, this.props.token)
+            .then(() => this.props.history.push('/success'))
+            .catch(err => console.log('Error: ', err));
+    }
 
-  render() {
-    const { loggedInName, bookId } = this.props;
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Title:
-          <input
-            name="bookTitle"
-            type="text"
-            placeholder="Book Title"
-            value={this.state.bookTitle}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          ISBN:
-          <input
-            name="isbnNumber"
-            type="text"
-            placeholder="978-3-16-148410-0"
-            value={this.state.isbnNumber}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Shelf:
-          <input
-            name="shelf"
-            type="text"
-            placeholder="A1"
-            value={this.state.shelf}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
+    render() {
+        const {loggedInName, bookId} = this.props;
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <table>
+                    <tr>
+                        <td>
+                            <label>Title</label>
+                        </td>
+                        <td>
+                            <input
+                                name="bookTitle"
+                                type="text"
+                                placeholder="Book Title"
+                                value={this.state.bookTitle}
+                                onChange={this.handleInputChange}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>ISBN</label>
+                        </td>
+                        <td>
+                            <input
+                                name="isbnNumber"
+                                type="text"
+                                placeholder="978-3-16-148410-0"
+                                value={this.state.isbnNumber}
+                                onChange={this.handleInputChange}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Shelf</label>
+                        </td>
+                        <td>
+                            <input
+                                name="shelf"
+                                type="text"
+                                placeholder="A1"
+                                value={this.state.shelf}
+                                onChange={this.handleInputChange}
+                            />
+                        </td>
+                    </tr>
+                </table>
+                <br/>
+                <input type="submit" value="Submit"/>
+            </form>
+        );
+    }
 }
