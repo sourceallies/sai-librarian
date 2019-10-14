5362503dd8d430b9beac47d8a7ec46765e088a9b
Maggie Pena <iglesiasmariangely@gmail.com>
2019-10-13 23:48:05 -0500

    Improved styling for 2 additional pages
    changed styling for AfterDetails and Shelf pages

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const Shelf = ({onDone, shelf, title}) => (
-    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start'}}>
-        <p>Return {title} to shelf {shelf}</p>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', marginTop: '40px'}}>
+        <h3>Return {title} to Shelf {shelf}</h3>
+     	<div style={{textAlign: 'center'}}>
+     		<p>To Continue, please return the book to the provided Shelf. </p>
+     	</div>
         <div style={{display: 'flex', alignItems: 'center'}}>
-            <button style={{margin: 4}} onClick={onDone}>Done</button>
+            <button style={{margin: 10}} onClick={onDone}>Continue</button>
         </div>
     </div>
 );
