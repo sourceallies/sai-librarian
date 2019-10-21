951aa618aad461d6b92cb36650d3e524d07946e1
Ben <benjamin.mchone@gmail.com>
2019-10-20 20:37:20 -0500

    Removed dead code (#30)
    * Removed dead code
    
    * Removed unneeded pages
    
    * Fixed eslint errors

--- src/utils/getBook.js
+++ src/utils/getBook.js
@@ -1,6 +1,6 @@
 import documentClient from '../configuredDocumentClient';
 
-export const getBookByBookId = (bookId, token) => {
+export const getBookByBookId = (bookId) => {
   return documentClient.get({
     TableName: process.env.REACT_APP_BOOK_TABLE,
     Key: {
