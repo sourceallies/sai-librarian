1c5b55c4dfebe0c2bcd231a6dff98a7c0f59396f
Paul Rowe <prowe@sourceallies.com>
2019-10-31 20:44:31 -0500

    wip

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Redirect, Route, Router, Switch } from 'react-router-dom';
-import wrapWithAuth from '../auth/wrap-with-auth';
+import AWS from 'aws-sdk';
+import { makeAuthenticator, Callback } from 'react-oidc';
+import userManager from '../auth/user-manager';
+import '../configuredDocumentClient';
 import Books from './Books';
-import OAuthCallback from './OAuthCallback';
 import BookList from "./BookList";
 import BookAddSuccess from "./BookAddSuccess";
 import BulkAddPage from './bulk-add/BulkAddPage';
 import GenerateLabelFile from './GenerateLabelFile';
 const { createBrowserHistory } = require('history');
+
+const wrapWithAuth = makeAuthenticator({userManager});
 
 const Routes = () => (
   <Router history={createBrowserHistory()}>
@@ -20,7 +24,16 @@ const Routes = () => (
       <Route path="/success" component={wrapWithAuth(BookAddSuccess)} />
       <Route path="/bulk-add" component={wrapWithAuth(BulkAddPage)} />
       <Route path="/label-file" component={wrapWithAuth(GenerateLabelFile)} />
-      <Route path="/implicit/callback" component={OAuthCallback} />
+      <Route path="/implicit/callback" render={({history}) => (
+        <Callback
+          userManager={userManager}
+          onSuccess={(user) => {
+            console.log('user authenticated', user);
+            AWS.config.credentials.params.WebIdentityToken = user.id_token;
+            history.push('/');
+          }}
+        />
+      )} />
     </Switch>
   </Router>
 );
