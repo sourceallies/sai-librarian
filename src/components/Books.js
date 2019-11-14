e8fc20d5f190dfabbcae2fe9bff4a4c3145c16b0
prowe <prowe@sourceallies.com>
2019-11-13 20:40:30 -0600

    Unit Testing (#59)
    * working on scan to add
    
    * making progress
    
    * this is looking good
    
    * styling
    
    * scanning wip
    
    * Testing setup
    
    * wip
    
    * green
    
    * refactoring to two components
    
    * adding extra properties to book
    
    * Auto submit on scan works
    
    * adding manual save support and fixing linter issues
    
    * adding a cloudfront origin
    
    * enabling unit testing
    
    * wip
    
    * wip
    
    * green
    
    * some refactoring
    
    * wrapping up book detail page
    
    * book create
    
    * testing the listing page
    
    * removing some moved components
    
    * fixing routes
    
    * some PR feedback
    
    * fixing a test

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,44 +1,107 @@
 import React, {useState, useEffect} from 'react';
-import {getBookByBookId} from "../utils/getBook";
-import BookDetail from "./BookDetail";
-import BookCreate from "./BookCreate";
+import documentClient from '../configuredDocumentClient';
 
-const Books = (props) => {
+const useGetBook = (bookId) => {
     const [loading, setLoading] = useState(true);
-    const [book, setBook] = useState({});
+    const [book, setBook] = useState();
+    const [error, setError] = useState();
 
     useEffect(() => {
-        getBookByBookId(props.match.params.id).then((data) => {
-            if (data.Item) {
-                setBook(data.Item);
+        documentClient.get({
+            TableName: process.env.REACT_APP_BOOK_TABLE,
+            Key: {
+              bookId
             }
-            setLoading(false);
-        }).catch((err) => console.log('Error: ', err));
-    }, [props.match.params.id]);
+        }).promise()
+            .then(({Item}) => setBook(Item))
+            .catch((e) => {
+                console.error(e);
+                setError(e);
+            })
+            .finally(() => setLoading(false));
+    }, [bookId]);
+
+    return {
+        loading,
+        error,
+        book,
+        setBook
+    };
+};
+
+const updateBookStatus = async (bookId, checkedOutBy) => {
+    return await documentClient.update({
+        TableName: process.env.REACT_APP_BOOK_TABLE,
+        Key: {
+            bookId
+        },
+        UpdateExpression: "set checkedOutBy=:l",
+        ExpressionAttributeValues: {
+            ':l': checkedOutBy
+        },
+        ReturnValues: 'UPDATED_NEW'
+    }).promise();
+};
+
+const AvailablityParagraph = ({book}) => {
+    if (book.checkedOutBy) {
+        return <p>Currently checked out by {book.checkedOutBy}</p>;
+    }
+    return <p>This book is available</p>;
+};
+
+const ShelfParagraph = ({book}) => {
+    if (book.checkedOutBy) {
+        return <p>Return this book to shelf {book.shelf}</p>;
+    }
+    return <p>This book is located on shelf {book.shelf}</p>;
+};
+
+const Books = ({match, history, user}) => {
+    const {loading, book, error, setBook} = useGetBook(match.params.id);
+    const [successMessage, setSuccessMessage] = useState();
 
     if (loading) {
-        return (<h1>Loading...</h1>);
+        return (<div>Loading...</div>);
     }
 
-    if (!book.bookId) {
-        return (
-        <BookCreate
-            bookId={props.match.params.id}
-            loggedInName={props.user.profile.name}
-            token={props.user.id_token}
-            history={props.history}
-        />
-        );
+    if (error) {
+        return (<div>Error: {error.message}</div>);
     }
+
+    if (!book) {
+        history.push(`/books/${match.params.id}/create`);
+        return null;
+    }
+
+    const onToggleAvailability = async () => {
+        const newCheckedOutBy = book.checkedOutBy ? null : user.profile.name;
+        const response = await updateBookStatus(book.bookId, newCheckedOutBy);
+        setBook((prevState) => {
+            return {
+                ...prevState,
+                ...response.Attributes
+            };
+        });
+        if (newCheckedOutBy) {
+            setSuccessMessage('Book successfully checked out');
+        } else {
+            setSuccessMessage('Book successfully returned');
+        }
+    };
 
     return (
-        <BookDetail
-            book={book}
-            loggedInName={props.user.profile.name}
-            history={props.history}
-            token={props.user.id_token}
-        />
+        <main>
+            <h1>{book.title} <small>{book.isbn}</small></h1>
+
+            <p>{successMessage}</p>
+
+            <AvailablityParagraph book={book} />
+            <ShelfParagraph book={book} />
+
+            <button onClick={onToggleAvailability}>{book.checkedOutBy ? 'Return' : 'Check Out'}</button>
+        </main>
     );
-    }
+};
 
 export default Books;
