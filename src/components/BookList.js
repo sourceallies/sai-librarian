cb230f4b58faa3b43cb4acc52c1fa8ea8f8ec9ba
Paul Rowe <prowe@sourceallies.com>
2019-11-26 21:11:50 -0600

    Revert "Merge branch 'master' of https://github.com/sourceallies/sai-librarian"
    This reverts commit 9f73ee6d2b1dc4c2f9a6026aa2a2be92543ac3b9, reversing
    changes made to 959d6a6f0aaab8ede7905e201dd8373b8113ed9f.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,75 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { MdDoNotDisturb, MdCheckCircle } from 'react-icons/md';
+import React, {useState, useEffect} from 'react';
+import {Link} from "react-router-dom";
+import { MdDoNotDisturb, MdCheckCircle } from "react-icons/md";
 import documentClient from '../configuredDocumentClient';
 import styles from './BookList.module.css';
-import Layout from './layout/Layout';
 
 async function getBookList() {
-  return await documentClient
-    .scan({
-      TableName: process.env.REACT_APP_BOOK_TABLE
-    })
-    .promise();
+    return await documentClient.scan({
+        TableName: process.env.REACT_APP_BOOK_TABLE
+    }).promise();
 }
 
-const AvailablilityIcon = ({ checkedOutBy }) => {
-  if (checkedOutBy) {
-    return (
-      <div className={styles.checkedOutIcon} title="Not Available">
-        <MdDoNotDisturb />
-      </div>
-    );
-  }
-  return (
-    <div className={styles.availableIcon} title="Available">
-      <MdCheckCircle />
-    </div>
-  );
-};
+const AvailablilityIcon = ({checkedOutBy}) => {
+    if (checkedOutBy) {
+        return <div className={styles.checkedOutIcon} title="Not Available"><MdDoNotDisturb/></div>;
+    }
+    return <div className={styles.availableIcon} title="Available"><MdCheckCircle/></div>;
+}
 
-const BookLink = props => {
-  const { bookId, title, checkedOutBy } = props.book;
-  return (
-    <li className={styles.listItem}>
-      <AvailablilityIcon checkedOutBy={checkedOutBy} />
-      <Link to={`/books/${bookId}`} className={styles.bookLink}>
-        {title}
-      </Link>
-    </li>
-  );
-};
+const BookLink = (props) => {
+    const { bookId, title, checkedOutBy } = props.book;
+    return (
+        <li className={styles.listItem}>
+            <AvailablilityIcon checkedOutBy={checkedOutBy} />
+            <Link to={`/books/${bookId}`} className={styles.bookLink}>{title}</Link>
+        </li>
+    );
+}
 
 const BookList = () => {
-  const [loading, setLoading] = useState(true);
-  const [bookList, setBookList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [bookList, setBookList] = useState([]);
 
-  const generateListOfBookDetails = () =>
-    bookList.map(book => <BookLink key={book.bookId} book={book} />);
+    const generateListOfBookDetails = () => bookList.map((book) =>
+        <BookLink key={book.bookId} book={book} />
+    );
 
-  useEffect(() => {
-    getBookList()
-      .then(data => {
-        setBookList(
-          data.Items.sort((a, b) => {
-            if (b.title > a.title) return -1;
-            if (a.title > b.title) return 1;
-            return 0;
-          })
-        );
-        setLoading(false);
-      })
-      .catch(err => console.log('Error: ', err));
-  }, []);
+    useEffect(() => {
+        getBookList().then((data) => {
+            setBookList(data.Items.sort((a, b) => {
+                if (b.title > a.title) return -1;
+                if (a.title > b.title) return 1;
+                return 0;
+            }));
+            setLoading(false);
+        }).catch((err) => console.log('Error: ', err));
+    }, []);
 
-  if (loading) {
-    return <Layout title="Loading...." />;
-  }
-  return (
-    <Layout title="Source Allies Library">
-      <ul className={styles.bookList}>{generateListOfBookDetails()}</ul>
-    </Layout>
-  );
-};
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+    return (
+        <div>
+            <h1>Source Allies Library</h1>
+            <ul className={styles.bookList}>
+                {generateListOfBookDetails()}
+            </ul>
+        </div>
+    );
+}
 
 export default BookList;
